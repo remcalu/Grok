951eff43cb34f27119b8f26584aff419923e23c5
Remus Calugarescu <59599955+remcalu@users.noreply.github.com>
2022-09-03 20:21:56 -0400

    Create 2-2.java

---
+++ 2-2.java
@@ -0,0 +1,19 @@
+class MaxSumSubArrayOfSizeK {
+  public static int findMaxSumSubArray(int k, int[] arr) {
+    int absMaxSum = 0;
+    int localMaxSum = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      localMaxSum += arr[i];
+      if (i >= k) {
+        localMaxSum -= arr[i-k];
+      }
+
+      if (absMaxSum < localMaxSum) {
+        absMaxSum = localMaxSum;
+      }
+    }
+
+    return absMaxSum;
+  }
+}
